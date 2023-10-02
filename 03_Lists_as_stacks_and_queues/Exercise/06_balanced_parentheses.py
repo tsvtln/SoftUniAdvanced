8f449f2cb5ac6866d65e18b239437d5a5353a261
TSVTLN <tsvtln@abv.bg>
2023-10-02 13:03:23 +0300

    Update 06_balanced_parentheses.py

---
+++ 03_Lists_as_stacks_and_queues/Exercise/06_balanced_parentheses.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+dq = deque(input())
+ob, cb, count = '([{', ')]}', 0
+
+while dq and count < len(dq) / 2:
+    if dq[0] not in ob:
+        break
+    index = ob.index(dq[0])
+    if dq[1] == cb[index]:
+        dq.popleft()
+        dq.popleft()
+        dq.rotate(count)
+        count = 0
+    else:
+        dq.rotate(-1)
+        count += 1
+
+if dq:
+    print('NO')
+else:
+    print('YES')
+    
