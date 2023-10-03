92853711690ef8d191f29ac577eeb03de0d20360
TSVTLN <tsvtln@abv.bg>
2023-10-03 15:13:32 +0300

    Update 03_milkshakes.py

--- 05_Tuples_and_Sets/Exercise_Stacks_Qs_Tuples_Sets/03_milkshakes.py
+++ 05_Tuples_and_Sets/Exercise_Stacks_Qs_Tuples_Sets/03_milkshakes.py
@@ -3,33 +3,27 @@ from collections import deque as dq
 choco, milk, sd = dq([int(x) for x in input().split(', ')]), dq([int(x) for x in input().split(', ')]), 0
 
 while sd != 5 and milk and choco:
-
     if milk[0] <= 0 and choco[-1] <= 0:
         milk.popleft()
         choco.pop()
         continue
-
     if choco[-1] <= 0:
         choco.pop()
         if len(choco) == 0:
             break
         continue
-
     if milk[0] <= 0:
         milk.popleft()
         if len(milk) == 0:
             break
         continue
-
     if choco[-1] == milk[0]:
         sd += 1
         choco.pop()
         milk.popleft()
-
     else:
         milk.rotate(-1)
         choco[-1] -= 5
-
 print("Great! You made all the chocolate milkshakes needed!" if sd >= 5 else "Not enough milkshakes.")
 print(f"Chocolate: {', '.join(map(str, choco))}" if choco else "Chocolate: empty")
 print(f"Milk: {', '.join(map(str, milk))}" if milk else "Milk: empty")
