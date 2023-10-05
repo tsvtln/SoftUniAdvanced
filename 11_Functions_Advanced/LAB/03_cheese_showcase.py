2b4e84592e20a5b524177abe23190b6de555f732
TSVTLN <tsvtln@abv.bg>
2023-10-05 22:53:00 +0300

    Update 03_cheese_showcase.py

--- 11_Functions_Advanced/LAB/03_cheese_showcase.py
+++ 11_Functions_Advanced/LAB/03_cheese_showcase.py
@@ -1,9 +1,21 @@
 def sorting_cheeses(**kwargs):
     sorted_dict = sorted(kwargs.items(), key=lambda kvp: (-len(kvp[1]), kvp[0]))
-    result = ''
+    result = ""
+
     for name, quantities in sorted_dict:
         result += f"{name}\n"
         sorted_quantities = sorted(quantities, reverse=True)
-        result += '\n'.join([str(el) for el in sorted_quantities])
+        result += "\n".join([str(el) for el in sorted_quantities])
         result += "\n"
-    return sorted_dict
+    return result
+
+"""
+test print
+"""
+# print(
+#     sorting_cheeses(
+#         Parmesan=[102, 120, 135],
+#         Camembert=[100, 100, 105, 500, 430],
+#         Mozzarella=[50, 125],
+#     )
+# )
