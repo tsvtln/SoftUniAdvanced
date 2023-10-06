0b63770c739ed529fbc62c8cbe551ce4c319dc37
TSVTLN <tsvtln@abv.bg>
2023-10-06 11:25:52 +0300

    Update 06_function_executor.py

---
+++ 11_Functions_Advanced/Exercise_Functions_advanced/06_function_executor.py
@@ -0,0 +1,20 @@
+def func_executor(*args):
+    result = []
+    for func, data in args:
+        result.append(f"{func.__name__} - {func(*data)}")
+    return "\n".join(result)
+
+
+"""
+test print
+"""
+# def sum_numbers(num1, num2):
+#     return num1 + num2
+#
+# def multiply_numbers(num1, num2):
+#     return num1 * num2
+#
+# print(func_executor(
+#     (sum_numbers, (1, 2)),
+#     (multiply_numbers, (2, 4))
+# ))
