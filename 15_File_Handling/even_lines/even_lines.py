0b3ed5782b6bb9537a5c94eaac4719ccb3d7378e
TSVTLN <tsvtln@abv.bg>
2023-10-10 14:36:03 +0300

    Update even_lines.py

--- 15_File_Handling/even_lines/even_lines.py
+++ 15_File_Handling/even_lines/even_lines.py
@@ -1,1 +1,1 @@
-[print(f"{line.replace('-', '@').replace(',', '@').replace('.', '@').replace('!', '@').replace('?', '@').strip()}") for i, line in enumerate(open("text.txt", "r").readlines()) if i % 2 == 0]
+[print(f"{' '.join(line.replace('-', '@').replace(',', '@').replace('.', '@').replace('!', '@').replace('?', '@').strip().split()[::-1])}") for i, line in enumerate(open("text.txt", "r").readlines()) if i % 2 == 0]
